e04008047b963ab4927f85cdea96b7e2a7e0510d
Daniel-Cocos <dev.danielcocos@gmail.com>
2026-08-21 14:57:19 +0100

    feat(model.py): Implemented last conv blocks and classifier

--- src/model.py
+++ src/model.py
@@ -1,9 +1,8 @@
-
 import torch.nn as nn
 
 
 class CIFAR10CNN(nn.Module):
-    def __init__(self, dropout: float = 0.25):
+    def __init__(self, num_classes: int = 10, dropout: float = 0.25):
         super().__init__()
 
         # Converting 32x32 to 16x16
@@ -17,7 +16,39 @@ class CIFAR10CNN(nn.Module):
             nn.MaxPool2d(2),                # halve the spatial size
             nn.Dropout(dropout),
         )
+        # --- Block 2: 16x16 -> 8x8 ---
+        self.block2 = nn.Sequential(
+            nn.Conv2d(32, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(64, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2),
+            nn.Dropout(dropout),
+        )
+        # --- Block 3: 8x8 -> 4x4 ---
+        self.block3 = nn.Sequential(
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(128, 128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2),
+            nn.Dropout(dropout),
+        )
+        self.classifier = nn.Sequential(
+            nn.Flatten(),                       # 128*4*4 -> 2048
+            nn.Linear(128 * 4 * 4, 512),
+            nn.ReLU(inplace=True),
+            nn.Dropout(dropout),
+            nn.Linear(512, num_classes),        # 10 raw scores (logits)
+        )
 
     def forward(self, x):
         x = self.block1(x)
+        x = self.block2(x)
+        x = self.block3(x)
+        x = self.clasifier(x)
         return x
