4566c3d76d57a9232414705fa4264c788a342670
Daniel-Cocos <dev.danielcocos@gmail.com>
2026-08-21 11:11:07 +0100

    feat(flake.nix): Enabled CUDA support to lower training time

--- flake.nix
+++ flake.nix
@@ -3,22 +3,30 @@
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
-    in {
-      devShells.${system}.default = pkgs.mkShell {
-        packages = with pkgs; [
-          python312
-          python312Packages.torch       # PyTorch core
-          python312Packages.torchvision # datasets + transforms
-          python312Packages.numpy
-          python312Packages.matplotlib  # plot images & curves
-          python312Packages.seaborn     # confusion matrix
-          python312Packages.pillow      # image handling
-          python312Packages.tensorboard # training viz
-        ];
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+      config = {
+        cudaSupport = true;
+        allowUnfree = true;
       };
     };
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      packages = with pkgs; [
+        python312
+        python312Packages.torch # PyTorch core
+        python312Packages.torchvision # datasets + transforms
+        python312Packages.numpy
+        python312Packages.matplotlib # plot images & curves
+        python312Packages.seaborn # confusion matrix
+        python312Packages.pillow # image handling
+        python312Packages.tensorboard # training viz
+      ];
+    };
+  };
 }
